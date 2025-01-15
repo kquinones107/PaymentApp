ef1188a16517b22d39365b9d506ef3a252eb8c8b
kquinones107 <kquinones@unal.edu.co>
2025-01-15 10:30:07 -0500

    feature: pantalla de productos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
     <Stack>
       <Stack.Screen name="index" options={{headerShown: false}} />
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
-      <Stack.Screen name="home" options={{ headerShown: false }} />
+      <Stack.Screen name="productsScreen" options={{ headerShown: false }} />
     </Stack>
   );
 
