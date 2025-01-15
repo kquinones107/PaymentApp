99cd41df479adf4cea3527185b43456377e0bd23
kquinones107 <kquinones@unal.edu.co>
2025-01-15 08:30:44 -0500

    feature: Ajustes de inicio

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,44 @@
-import { Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Text, View, Image, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/home");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/logo.jpg")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 16,
+    color: "#666",
+  },
+});
