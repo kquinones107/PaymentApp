82ee08ea7c2005ec16c553b20f9dad1a2edaa9a6
kquinones107 <kquinones@unal.edu.co>
2025-01-17 11:17:54 -0500

    feature:  se creo select producto

---
+++ src/redux/slices/productSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { act } from "react";
+
+interface Product {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+interface ProductState {
+    products: Product[];
+}
+
+const initialState: ProductState = {
+    products: [],
+};
+
+const productSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {
+        addProduct(state, action: PayloadAction<Product>) {
+            const existingproduct = state.products.find(
+                (product) => product.id === action.payload.id
+            );
+            if (existingproduct) {
+                existingproduct.quantity+= action.payload.quantity;
+            } else {
+                state.products.push(action.payload );
+            }
+        },
+        removeProduct(state, action: PayloadAction<string>) {
+            state.products = state.products.filter(
+                (product) => product.id === action.payload
+            );
+        },
+        clearProducts(state) {
+            state.products = [];
+        },
+    },
+}); 
+
+export const { addProduct, removeProduct, clearProducts } = productSlice.actions;
+
+export default productSlice.reducer;
