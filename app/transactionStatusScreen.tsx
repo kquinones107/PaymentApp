5e6b6938b2ee44f4c6b435b2444d6916003d1932
kquinones107 <kquinones@unal.edu.co>
2025-01-19 16:38:25 -0500

    feature: Estado Fial de la transacción

---
+++ app/transactionStatusScreen.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import Button from "@/components/Button";
+import { useRouter } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
+import { RootState } from "@/redux/store";
+
+export default function TransactionStatusScreen() {
+  const router = useRouter();
+  const { status } = useLocalSearchParams(); // Recibir parámetro "status" (éxito o error)
+
+  const isSuccess = status === "success"; // Determina si fue éxito o error
+
+  return (
+    <View style={styles.container}>
+      {/* Mensaje dinámico basado en el estado */}
+      {isSuccess ? (
+        <Image
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
+      ) : (
+        <Image
+          source={require("../assets/images/error.jpg")}
+          style={styles.image}
+        />
+      )}
+
+      <Text style={styles.title}>
+        {isSuccess ? "¡Transacción Exitosa!" : "Transacción Fallida"}
+      </Text>
+      <Text style={styles.message}>
+        {isSuccess
+          ? "Tu pago ha sido procesado correctamente."
+          : "Hubo un problema procesando tu pago. Por favor, inténtalo nuevamente."}
+      </Text>
+
+      <Button
+        title="Volver a Productos"
+        onPress={() => router.push("/productsScreen")} // Navegar a la pantalla de productos
+        style={styles.button}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 16,
+    textAlign: "center",
+    color: "#666",
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 20,
+    padding: 15,
+    borderRadius: 10,
+  },
+});
