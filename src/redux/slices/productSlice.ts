0c041c9626f7530fe81c8a2330b9cc4facf3ee4f
kquinones107 <kquinones@unal.edu.co>
2025-01-19 22:05:07 -0500

    fix: Ajustando últimos detalles

--- src/redux/slices/productSlice.ts
+++ src/redux/slices/productSlice.ts
@@ -31,9 +31,18 @@ const productSlice = createSlice({
         }
       },
       removeProduct: (state, action: PayloadAction<string>) => {
-        state.products = state.products.filter(
-          (product) => product.id !== action.payload
+        const productIndex = state.products.findIndex(
+          (product) => product.id === action.payload
         );
+
+        if (productIndex !== -1) {
+          const product = state.products[productIndex];
+          if (product.quantity > 1) {
+            product.quantity -= 1; // Disminuir la cantidad
+          } else {
+            state.products.splice(productIndex, 1); // Eliminar el producto si la cantidad es 1
+          }
+        }
       },
     },
   });
